0a73bd47f0dfe925c2fa3c3a309db8eb114d0772
Jay Shah <jaymshah3@gmail.com>
2020-03-28 01:44:20 -0400

    data structures of hand + test file

--- Test.py
+++ Test.py
@@ -1,20 +1,13 @@
 from card import Card
 from hand import Hand
 c = []
-c.append(Card(3,'c'))
-c.append(Card(4,'c'))
+c.append(Card(3,'d'))
+c.append(Card(4,'d'))
 c.append(Card(5,'c'))
 c.append(Card(6,'c'))
 c.append(Card(7,'c'))
 h = Hand(c)
-print(c[0])
 print(h.max_card)
 print(h.is_flush)
-def find_is_straight(hand):
-    for i in range(len(hand.cards)-1):
-         if hand.cards[i].value > hand.cards[i+1].value:
-            return False
-    return True
-print(find_is_straight(h))
-
+print(h.is_straight)
 
--- hand.py
+++ hand.py
@@ -1,34 +1,38 @@
 import card
 class Hand():
+
     def __init__(self, cards):
         self.cards = sorted(cards, key= lambda card:card.value)
         self.max_card = cards[4]
         self.is_straight = False
         self.is_flush = False
-        #self.major_group = find_major_group()
+        self.major_group = -1
         self.value_map = {}
         self.suit_map = {'s': 0,'h' :0,'c':0,'d':0}
-        
-        def find_is_flush(self):
-            for keys in self.suitMap:
-                if self.suitMap[keys] == 4:
-                    self.is_flush = True
-
-        def find_is_straight(self):
-            for i in range(len(self.cards)-1):
-                if self.cards[i].value > self.cards[i+1].value:
-                    self.is_straight = False
+        self.find_major_group()
+        self.find_is_flush()
+        self.find_is_straight()
 
 
-        def find_major_group(self):
-            for card in self.cards:
-                if card.value in self.value_map:
-                    self.value_map[card.val]+=1
-                else:
-                    self.value_map[card.value] = 1
-                self.suitMap[card.suit]+=1
-            return 1
+    def find_is_flush(self):
+        for keys in self.suit_map:
+            if self.suit_map[keys] == 5:
+                self.is_flush = True
 
+    def find_is_straight(self):
+        for i in range(len(self.cards)-1):
+            if self.cards[i+1].value - self.cards[i].value > 1:
+                self.is_straight = False
+                return
+        self.is_straight = True
+
+    def find_major_group(self):
+        for card in self.cards:
+            if card.value in self.value_map:
+                 self.value_map[card.val]+=1
+            else:
+                self.value_map[card.value] = 1
+            self.suit_map[card.suit]+=1
     
 
             
