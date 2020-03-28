3baa2d3dcfa23d42d7eb7ca5ff0ef3d972de14b3
Jay Shah <jaymshah3@gmail.com>
2020-03-28 02:34:13 -0400

    i accidently over wrote card, reverted

--- Test.py
+++ Test.py
@@ -1,11 +1,11 @@
 from card import Card
 from hand import Hand
 c = []
-c.append(Card(3,'d'))
-c.append(Card(4,'d'))
-c.append(Card(5,'c'))
-c.append(Card(6,'c'))
-c.append(Card(7,'c'))
+c.append(Card(12,'c'))
+c.append(Card(13,'c'))
+c.append(Card(1,'c'))
+c.append(Card(10,'c'))
+c.append(Card(11,'c'))
 h = Hand(c)
 print(h.max_card)
 print(h.is_flush)
--- hand.py
+++ hand.py
@@ -3,15 +3,24 @@ class Hand():
 
     def __init__(self, cards):
         self.cards = sorted(cards, key= lambda card:card.value)
-        self.max_card = cards[4]
+        for b in self.cards:
+            print(b.value)
+        if self.cards[0].value == 1:
+            self.max_card = self.cards[0]
+        else:
+            self.max_card = self.cards[4]
         self.is_straight = False
         self.is_flush = False
         self.major_group = -1
         self.value_map = {}
         self.suit_map = {'s': 0,'h' :0,'c':0,'d':0}
         self.find_major_group()
-        self.find_is_flush()
-        self.find_is_straight()
+        for k,v in self.value_map.items():
+            print(k)
+            print(v)
+        for k,v in self.suit_map.items():
+            print(k)
+            print(v)
 
 
     def find_is_flush(self):
@@ -20,19 +29,34 @@ class Hand():
                 self.is_flush = True
 
     def find_is_straight(self):
-        for i in range(len(self.cards)-1):
-            if self.cards[i+1].value - self.cards[i].value > 1:
-                self.is_straight = False
+        if self.max_card.value == 1: # if there is an ace, checking a straight's a bit different
+            if self.cards[1].value == 10 and self.cards[2].value == 11 and self.cards[3].value == 12 and self.cards[4].value == 13:
+                self.is_straight = True
                 return
+            else:
+                for i in range(2,len(self.cards)-1):
+                    if self.cards[i+1].value - self.cards[i].value != 1:
+                        self.is_straight = False
+                        return
+        else:    #normal way to check a straight with no ace 
+            for i in range(len(self.cards)-1):
+                if self.cards[i+1].value - self.cards[i].value != 1:
+                    self.is_straight = False
+                    return
         self.is_straight = True
 
     def find_major_group(self):
         for card in self.cards:
             if card.value in self.value_map:
-                 self.value_map[card.val]+=1
+                 self.value_map[card.value]+=1
             else:
                 self.value_map[card.value] = 1
             self.suit_map[card.suit]+=1
-    
+        self.find_is_flush()
+        self.find_is_straight() 
+        if self.is_flush and self.is_straight and self.max_card.value == 1:
+            print('royal flush')
+        elif self.is_flush and self.is_straight:
+            print('straight flush')
 
             
