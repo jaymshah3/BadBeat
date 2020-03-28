8bf6d4a9dc7d9fd806e82f3f077029e08f631bf9
Aditya Shastri <ad.shastri1@gmail.com>
2020-03-28 16:28:16 -0400

    pass more vars in

--- hand.py
+++ hand.py
@@ -17,12 +17,14 @@ class Hand():
         suit_map = {'s': 0,'h' :0,'c':0,'d':0}
         return find_major_group(cards, value_map, suit_map, max_card)
 
-    def __init__(self, num, cards, value_map, suit_map, max_card):
+    def __init__(self, num, cards, value_map, suit_map, max_card, is_straight, is_flush):
         self.major_group = num
         self.cards = cards
         self.value_map = value_map
         self.suit_map = suit_map
         self.max_card = max_card
+        self.is_straight = is_straight
+        self.is_flush = is_flush
 
     @staticmethod
     def find_is_flush(suit_map):
@@ -59,34 +61,34 @@ class Hand():
         is_straight = find_is_straight(cards, max_card) 
         if is_flush and is_straight and max_card.value == 1 and cards[4].value == 13:
             print('royal flush')
-            return RoyalFlush(10, cards, value_map, suit_map, max_card)
+            return RoyalFlush(10, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif is_flush and is_straight:
             print('straight flush')
-            return StraightFlush(9, cards, value_map, suit_map, max_card)
+            return StraightFlush(9, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif 4 in value_map.values():
             print('four of a kind')
-            return FourOfAKind(8, cards, value_map, suit_map, max_card)
+            return FourOfAKind(8, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif 3 in value_map.values() and 2 in value_map.values():
             print('full house')
-            return FullHouse(7, cards, value_map, suit_map, max_card)
+            return FullHouse(7, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif is_flush:
             print('flush')
-            return Flush(6, cards, value_map, suit_map, max_card)
+            return Flush(6, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif is_straight:
             print('straight')
-            return Straight(5, cards, value_map, suit_map, max_card)
+            return Straight(5, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif 3 in value_map.values():
             print('three of a kind')
-            return ThreeOfAKind(4, cards, value_map, suit_map, max_card)
+            return ThreeOfAKind(4, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif len({k:v for k,v in value_map.items() if v==2}) is 2:
             print('two pair')
-            return TwoPair(3, cards, value_map, suit_map, max_card)
+            return TwoPair(3, cards, value_map, suit_map, max_card, is_straight, is_flush)
         elif 2 in value_map.values():
             print('one pair')
-            return Pair(2, cards, value_map, suit_map, max_card)
+            return Pair(2, cards, value_map, suit_map, max_card, is_straight, is_flush)
         else:
             print('high card')
-            return HighCard(1, cards, value_map, suit_map, max_card)
+            return HighCard(1, cards, value_map, suit_map, max_card, is_straight, is_flush)
 
     def __lt__(self, other):
         if type(self) != type(other):
